394be5be58b678b2564509a43b50793d5bd64fdb
rickmaclean <rick_maclean@sil.org>
2017-04-10 14:11:13 -0500

    added progress bar and some other stuff

--- process/js/LoginSubcomponent.js
+++ process/js/LoginSubcomponent.js
@@ -6,7 +6,7 @@ var LoginSubcomponent = React.createClass({
   localHandleLogin: function (e) {
     //note we will be acccessing the input fields via the ref attributes
     // eg ref="inputOwnerName"
-    //note it receives an event handler  e   
+    //note it receives an event handler  e
     //(note all functions have e passed to it but we might not use it as in localToggleAptDisplay)
     var tempLoginCredentials = {
       userName: this.refs.inputEmail.value,
@@ -18,7 +18,7 @@ var LoginSubcomponent = React.createClass({
             //normal behavior is to cause a reload and sending of the information to a server
     this.props.subHandleLogin(tempLoginCredentials);  //pass tempItem to this prop in main
   }, //localHandleLogin
-  
+
 
    render: function() {
     return(
@@ -28,14 +28,14 @@ var LoginSubcomponent = React.createClass({
         <div className="panel-body">
           <form className="form-signin form-horizontal" onSubmit={this.localHandleLogin}>
             <div className="form-group">
-              <label className="col-sm-2 control-label" for="inputEmail">Username</label>
+              <label className="col-sm-2 control-label" htmlFor="inputEmail">Username</label>
               <div className="col-sm-10">
                 <input type="email" className="form-control"
                   id="inputEmail" ref="inputEmail" placeholder="Email address" />
               </div>
             </div>
             <div className="form-group">
-              <label className="col-sm-2 control-label" for="inputPassword">Password</label>
+              <label className="col-sm-2 control-label" htmlFor="inputPassword">Password</label>
               <div className="col-sm-10">
                 <input type="password" className="form-control"
                   id="inputPassword" ref="inputPassword" placeholder="Password" />
@@ -46,7 +46,7 @@ var LoginSubcomponent = React.createClass({
                         <input type="checkbox" value="remember-me"/> Remember me
                     </label>
                 </div>
-            
+
 
             <div className="form-group">
               <div className="col-sm-offset-2 col-sm-10">
--- process/js/app.js
+++ process/js/app.js
@@ -2,73 +2,167 @@ var React = require('react');
 var ReactDOM = require('react-dom');
 var _ = require('lodash');
 
-var AptList = require('./AptList');
+//var Line = require('rc-progress').Line;
+//var Circle = require('rc-progress').Circle;
+
 var LoginSubcomponent = require('./LoginSubcomponent');
+var JobSpecification = require('./JobSpecification');
+var ProgressElement = require('./ProgressBar');
+//var ReactPercentageCircle = require('./ReactPercentageCircle');
+
+function persistData(storage_key, jsonData) {
+  console.log('inside persitComponent() and storage_key is ' + storage_key + ', and jsonData is, ' + jsonData);
+    const appStorage = require('electron-json-storage');
+    //Write
+    appStorage.set(storage_key, jsonData, function (error) {
+        if (error) throw error;
+    });
+}
+
+function isNonemptyString(str) {
+  if (typeof str === 'string' && str.length > 0) {
+      return true;
+  }
+  else {
+    return false;
+  }
+}
+
+//var classSet        = require('./utils/classSet');
+//var ProgressElement = require('./components/Progress');
+//var JobSpecification = require('./Rc-progress');
+//var ProgressBar = require('./ProgressBar');
+
+var jsonData =   '{   "loginPersistKey": "Buffy@dogsplace.uk" , "metaDataPersistKey" : "c-path-pathtofilesFOLDER", "jobFilePersistKey" : "c-path-jobfilename"}';
+var jsonToObjData = JSON.parse(jsonData);
 
 var MainInterface = React.createClass({
   getInitialState: function() {
     return {
-      myAppointments: [],
       emailUsername: '',
-      password: ''
+        password: '',
+        metaDataFolder: 'metaDataFolder',
+        metaDataFolderSelected: false,
+        jobFilepath: 'jobFilepath',
+        jobFilepathSelected: false,
+        currentByteCount: 0,
+        totalByteCount: 0,
+        userKey: '',
+        errorMessage: '',
+        electronJsonStoredValue: '',
+        persistedData: jsonToObjData,
+        percentComplete: 15
     } //return
   }, //getInitialState
 
   componentDidMount: function() {
-    this.serverRequest = $.get('./js/data.json', function(result) {
-      var tempApts = result;
-      this.setState({
-        myAppointments: tempApts
-      }); //setState
-    }.bind(this));
+    console.log('componentDidMount lifecycle running');
+
+    var data = this.state.persistedData;
+
+      console.log('inside onestorePersistedAllData');
+      console.log('data.loginPersistKey is =>' +data.loginPersistKey);
+      console.log('data.metaDataPersistKey is =>' +data.metaDataPersistKey);
+      console.log('data.jobFilePersistKey is =>' +data.jobFilePersistKey);
+      // console.log('ALL the data restored from electron-json-storage  is : ' + data);
+      jQuery('#inputEmail').val(data.loginPersistKey);
+
+      if (isNonemptyString(data.metaDataPersistKey) ) {
+        jQuery('#metaDataFolderId').text(data.metaDataPersistKey);
+        this.setState( { metaDataFolderSelected : true }); //setState
+      }
+
+      if (isNonemptyString(data.jobFilePersistKey) ) {
+        jQuery('#jobFilePathId').text(data.jobFilePersistKey);
+        this.setState( { jobFilepathSelected : true }); //setState
+      }
+
+      if (isNonemptyString(data.jobFilePersistKey) && isNonemptyString(data.metaDataPersistKey)) {
+        $('#sendButton').removeAttr("disabled");
+      }
+
   }, //componentDidMount
 
   componentWillUnmount: function() {
     this.serverRequest.abort();
   }, //componentWillUnmount
 
-  deleteMessage: function(item) {
-    var allApts = this.state.myAppointments;
-    var newApts = _.without(allApts, item);
-    this.setState({
-      myAppointments: newApts
-    }); //setState
-  }, //deleteMessage
+  componentDidUpdate: function() {
+    console.log('componentDidUpdate lifecycle running');
+    console.log('this.state.metaDataFolder is =>' + this.state.metaDataFolder);
+    console.log('this.state.metaDataFolderSelected is =>' + this.state.metaDataFolderSelected);
+    console.log('this.state.jobFilepath is =>' + this.state.jobFilepath);
+    console.log('this.state.jobFilepathSelected is =>' + this.state.jobFilepathSelected);
+}, //componentDidUpdate
 
   mainHandleLogin: function(loginCredentials) {
     var subuserName = loginCredentials.userName;
     var subpassword = loginCredentials.password;
-    console.log(subuserName);
-    console.log(subpassword);
-    /*this.setState( {      
-      emailUsername : subuserName,  
+    console.log('subuserName is = ' + subuserName);
+    console.log('subpassword is = '  + subpassword);
+    /*this.setState( {
+      emailUsername : subuserName,
       password: subpassword
     }); //setState */
-    this.setState( {      
+    this.setState( {
       emailUsername : subuserName,
       password : subpassword
-      }); //setState 
+      }); //setState
+    //persistComponent('loginPersistKey', subuserName);
   }, //mainHandleLogin
 
+  onSelectMetaDataFile: function () {
+    console.log('called onSelectMetaDataFile');
+          this.setState({ metaDataFolderSelected: true });
+          this.setState({  metaDataFolder: 'c-path-pathtofilesFOLDER' });
+      console.log('end of onSelectMetaDataFile');
+  },
+
+  onSelectJobSpecsFile: function () {
+    console.log('called onSelectJobSpecsFile');
+        this.setState({ jobFilepathSelected: true });
+        this.setState({ jobFilepath: 'c-path-jobfilename' });
+      console.log('end of onSelectJobSpecsFile');
+  },
+
+    onHandleSend: function(sendFileSpecs) {
+      console.log('called onHandleSend');
+      console.log('metaDataFolder= ' + sendFileSpecs.metaDataFolder);
+      console.log('jobFilepath= ' + sendFileSpecs.jobFilepath);
+      var temp_percentComplete = this.state.percentComplete;
+      if (temp_percentComplete == 100)
+        { temp_percentComplete = 20; }
+      temp_percentComplete = temp_percentComplete + 0.33*temp_percentComplete;
+      if (temp_percentComplete > 100) {
+          this.setState({ percentComplete: 100}); }
+        else {
+          this.setState({ percentComplete: temp_percentComplete});
+        }
+  }, //onHandleSend
+
   render: function() {
-    var filteredApts = this.state.myAppointments;
-    filteredApts = filteredApts.map(function(item, index) {
-      return(
-        <AptList key = { index }
-          singleItem = { item }
-          whichItem = { item }
-          onDelete = { this.deleteMessage } />
-      ) //return
-    }.bind(this)); //filteredApts.map
+
+    $('#sendButton').removeAttr("disabled");
+
+
     return (
       <div className="interface">
-        <LoginSubcomponent 
+        <LoginSubcomponent
         subHandleLogin = {this.mainHandleLogin}
         subUsername = {this.state.emailUsername}
         subPassword = {this.state.password}
         />
-        <ul className="item-list media-list">{filteredApts}</ul>
+        <JobSpecification
+            metadataFilepath = {this.state.metadataFilepath}
+            jobFilepath = {this.state.jobFilepath}
+            onselectMetaDataFile = {this.onSelectMetaDataFile}
+            onselectJobSpecsFile = {this.onSelectJobSpecsFile}
+            handleSend = {this.onHandleSend}
+        />
+        <ProgressElement
+              percentComplete={this.state.percentComplete} />
       </div>
+
     ) //return
   } //render
 }); //MainInterface
