9307dcb826330455c8a2a287286d93a27581a00d
Richard MacLean <rick_maclean@sil.org>
2017-11-16 15:20:38 -0600

    changes for dbl

--- process/js/app.js
+++ process/js/app.js
@@ -2,120 +2,173 @@ var React = require('react');
 var ReactDOM = require('react-dom');
 var _ = require('lodash');
 
-var AptList = require('./AptList');
-var AddAppointment = require('./AddAppointment');
-var SearchAppointments = require('./SearchAppointments');
+
+//var Line = require('rc-progress').Line;
+//var Circle = require('rc-progress').Circle;
+
+var LoginSubcomponent = require('./LoginSubcomponent');
+var JobSpecification = require('./JobSpecification');
+var ProgressElement = require('./ProgressBar');
+//var ReactPercentageCircle = require('./ReactPercentageCircle');
+
+function persistData(storage_key, jsonData) {
+  console.log('inside persitComponent() and storage_key is ' + storage_key + ', and jsonData is, ' + jsonData);
+    const appStorage = require('electron-json-storage');
+    //Write
+    appStorage.set(storage_key, jsonData, function (error) {
+        if (error) throw error;
+    });
+}
+
+function isNonemptyString(str) {
+  if (typeof str === 'string' && str.length > 0) {
+      return true;
+  }
+  else {
+    return false;
+  }
+}
+
+//var classSet        = require('./utils/classSet');
+//var ProgressElement = require('./components/Progress');
+//var JobSpecification = require('./Rc-progress');
+//var ProgressBar = require('./ProgressBar');
+
+var jsonData =   '{   "loginPersistKey": "Buffy@dogsplace.uk" , "metaDataPersistKey" : "c-path-pathtofilesFOLDER", "jobFilePersistKey" : "c-path-jobfilename"}';
+var jsonToObjData = JSON.parse(jsonData);
 
 var MainInterface = React.createClass({
   getInitialState: function() {
     return {
-      aptBodyVisible: false,
-      orderBy: 'petName',
-      orderDir: 'asc',
-      queryText: '',
-      myAppointments: []
+      emailUsername: '',
+        password: '',
+        metaDataFolder: 'metaDataFolder',
+        metaDataFolderSelected: false,
+        jobFilepath: 'jobFilepath',
+        jobFilepathSelected: false,
+        currentByteCount: 0,
+        totalByteCount: 0,
+        userKey: '',
+        errorMessage: '',
+        electronJsonStoredValue: '',
+        persistedData: jsonToObjData,
+        percentComplete: 15
     } //return
   }, //getInitialState
 
   componentDidMount: function() {
-    this.serverRequest = $.get('./js/data.json', function(result) {
-      var tempApts = result;
-      this.setState({
-        myAppointments: tempApts
-      }); //setState
-    }.bind(this));
+    console.log('componentDidMount lifecycle running');
+
+    var data = this.state.persistedData;
+
+      console.log('inside onestorePersistedAllData');
+      console.log('data.loginPersistKey is =>' +data.loginPersistKey);
+      console.log('data.metaDataPersistKey is =>' +data.metaDataPersistKey);
+      console.log('data.jobFilePersistKey is =>' +data.jobFilePersistKey);
+      // console.log('ALL the data restored from electron-json-storage  is : ' + data);
+      jQuery('#inputEmail').val(data.loginPersistKey);
+
+      if (isNonemptyString(data.metaDataPersistKey) ) {
+        jQuery('#metaDataFolderId').text(data.metaDataPersistKey);
+        this.setState( { metaDataFolderSelected : true }); //setState
+      }
+
+      if (isNonemptyString(data.jobFilePersistKey) ) {
+        jQuery('#jobFilePathId').text(data.jobFilePersistKey);
+        this.setState( { jobFilepathSelected : true }); //setState
+      }
+
+      if (isNonemptyString(data.jobFilePersistKey) && isNonemptyString(data.metaDataPersistKey)) {
+        $('#sendButton').removeAttr("disabled");
+      }
+
   }, //componentDidMount
 
   componentWillUnmount: function() {
     this.serverRequest.abort();
   }, //componentWillUnmount
 
-  deleteMessage: function(item) {
-    var allApts = this.state.myAppointments;
-    var newApts = _.without(allApts, item);
-    this.setState({
-      myAppointments: newApts
-    }); //setState
-  }, //deleteMessage
+  componentDidUpdate: function() {
+    console.log('componentDidUpdate lifecycle running');
+    console.log('this.state.metaDataFolder is =>' + this.state.metaDataFolder);
+    console.log('this.state.metaDataFolderSelected is =>' + this.state.metaDataFolderSelected);
+    console.log('this.state.jobFilepath is =>' + this.state.jobFilepath);
+    console.log('this.state.jobFilepathSelected is =>' + this.state.jobFilepathSelected);
+}, //componentDidUpdate
 
-  toggleAddDisplay: function() {
-    var tempVisibility = !this.state.aptBodyVisible;
-    this.setState({
-      aptBodyVisible: tempVisibility
-    }); //setState
-  }, //toggleAddDisplay
+  mainHandleLogin: function(loginCredentials) {
+    var subuserName = loginCredentials.userName;
+    var subpassword = loginCredentials.password;
+    console.log('subuserName is = ' + subuserName);
+    console.log('subpassword is = '  + subpassword);
+    /*this.setState( {
+      emailUsername : subuserName,
+      password: subpassword
+    }); //setState */
+    this.setState( {
+      emailUsername : subuserName,
+      password : subpassword
+      }); //setState
+    //persistComponent('loginPersistKey', subuserName);
+  }, //mainHandleLogin
 
-  addItem: function(tempItem) {
-    var tempApts = this.state.myAppointments;
-    tempApts.push(tempItem);
-    this.setState({
-      myAppointments: tempApts
-    }); //setState
-  }, //addItem
+  onSelectMetaDataFile: function () {
+    console.log('called onSelectMetaDataFile');
+          this.setState({ metaDataFolderSelected: true });
+          this.setState({  metaDataFolder: 'c-path-pathtofilesFOLDER' });
+      console.log('end of onSelectMetaDataFile');
+  },
 
-  reOrder: function(orderBy, orderDir) {
-    this.setState({
-      orderBy: orderBy,
-      orderDir: orderDir
-    }); //setState
-  }, //reOrder
+  onSelectJobSpecsFile: function () {
+    console.log('called onSelectJobSpecsFile');
+        this.setState({ jobFilepathSelected: true });
+        this.setState({ jobFilepath: 'c-path-jobfilename' });
+      console.log('end of onSelectJobSpecsFile');
+  },
 
-  searchApts(q) {
-    this.setState({
-      queryText: q
-    }); //setState
-  }, //searchApts
+    onHandleSend: function(sendFileSpecs) {
+      console.log('called onHandleSend');
+      console.log('metaDataFolder= ' + sendFileSpecs.metaDataFolder);
+      console.log('jobFilepath= ' + sendFileSpecs.jobFilepath);
+      var temp_percentComplete = this.state.percentComplete;
+      if (temp_percentComplete == 100)
+        { temp_percentComplete = 20; }
+      temp_percentComplete = temp_percentComplete + 0.33*temp_percentComplete;
+      if (temp_percentComplete > 100) {
+          this.setState({ percentComplete: 100}); }
+        else {
+          this.setState({ percentComplete: temp_percentComplete});
+        }
+  }, //onHandleSend
 
   render: function() {
-    var filteredApts = [];
-    var orderBy = this.state.orderBy;
-    var orderDir = this.state.orderDir;
-    var queryText = this.state.queryText;
-    var myAppointments = this.state.myAppointments; 
 
-    myAppointments.forEach(function(item) {
-      if(
-        (item.petName.toLowerCase().indexOf(queryText)!=-1) ||
-        (item.ownerName.toLowerCase().indexOf(queryText)!=-1) ||
-        (item.aptDate.toLowerCase().indexOf(queryText)!=-1) ||
-        (item.aptNotes.toLowerCase().indexOf(queryText)!=-1)
-      ) {
-        filteredApts.push(item);
-      }
-    }); //forEach
+    $('#sendButton').removeAttr("disabled");
 
-    filteredApts = _.orderBy(filteredApts, function(item) {
-      return item[orderBy].toLowerCase();
-    }, orderDir);//orderBy
 
-    filteredApts = filteredApts.map(function(item, index) {
-      return(
-        <AptList key = { index }
-          singleItem = { item }
-          whichItem = { item }
-          onDelete = { this.deleteMessage } />
-      ) //return
-    }.bind(this)); //filteredApts.map
     return (
       <div className="interface">
-        <AddAppointment
-          bodyVisible = { this.state.aptBodyVisible }
-          handleToggle = { this.toggleAddDisplay }
-          addApt = { this.addItem }
+        <LoginSubcomponent
+        subHandleLogin = {this.mainHandleLogin}
+        subUsername = {this.state.emailUsername}
+        subPassword = {this.state.password}
         />
-        <SearchAppointments
-          orderBy = { this.state.orderBy }
-          orderDir = { this.state.orderDir }
-          onReOrder = { this.reOrder }
-          onSearch = { this.searchApts }
+        <JobSpecification
+            metadataFilepath = {this.state.metadataFilepath}
+            jobFilepath = {this.state.jobFilepath}
+            onselectMetaDataFile = {this.onSelectMetaDataFile}
+            onselectJobSpecsFile = {this.onSelectJobSpecsFile}
+            handleSend = {this.onHandleSend}
         />
-        <ul className="item-list media-list">{filteredApts}</ul>
+        <ProgressElement
+              percentComplete={this.state.percentComplete} />
       </div>
+
     ) //return
   } //render
 }); //MainInterface
 
 ReactDOM.render(
   <MainInterface />,
-  document.getElementById('petAppointments')
+  document.getElementById('ubsUploads')
 ); //render
